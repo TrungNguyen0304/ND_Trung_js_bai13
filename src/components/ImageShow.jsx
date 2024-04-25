6fdab63cec733ca22c0b66c09e834ffca57b4d48
TrungNguyen0304 <TrungNguyen15989@gmail.com>
2024-04-25 13:27:38 +0700

    first commit

---
+++ src/components/ImageShow.jsx
@@ -0,0 +1,11 @@
+import "./styleImageShow.css"
+
+const ImageShow = ({ image }) => {
+    return (
+      <div className="item">
+      <img src={image} />
+    </div>
+    )
+  }
+  
+  export default ImageShow
